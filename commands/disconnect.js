e20633a07e1ed87fcfb8f7503a576d08f78ea605
George <heyitsloth@outlook.com>
2022-03-17 17:57:57 +0000

    The same command twice!

---
+++ commands/disconnect.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('disconnect')
+        .setDescription('Disconnects the bot and kills the queue'),
+    async execute(interaction, player, DB) {
+        const checkRestrict = await DB.findOne({where:{name:'music'}});
+		if (checkRestrict) {
+			const restrictChannel = await interaction.guild.channels.fetch(checkRestrict.channel);
+			if (restrictChannel != interaction.channel.id) {
+				return interaction.reply({content: `❌ | The music commands are restricted to the ${restrictChannel.name} channel!`, ephemeral: true})
+			}
+		}
+
+        const queue = player.getQueue(interaction.guildId);
+        if (!queue || !queue.playing) return void interaction.reply('❌ | No music is being played!');
+        const success = queue.destroy();
+        return void interaction.reply({
+            content: !success ? `✅ | Successfully disconnected!` : '❌ | Something went wrong...'
+        });   
+    }
+};
