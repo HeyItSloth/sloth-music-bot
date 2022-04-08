93380c08b661cc3d834e6f412283c99057f0404b
George <heyitsloth@outlook.com>
2022-04-08 18:37:11 +0100

    /cat

---
+++ commands/cat.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fetch = require('node-fetch');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('cat')
+		.setDescription('Gib cat'),
+	async execute(interaction) {
+		const response = await fetch("https://aws.random.cat/meow");
+		console.log(response.ok)
+		if (response.ok) {
+			const {file} =  await response.json();
+			interaction.reply({ files: [file] });
+		} else {
+			interaction.reply("Shit's brokey, yo");
+		};
+	},
+};
